26a52835d771f66e3990a1f06f0d226d1dc68062
Justin <justinjones38@myyahoo.com>
2026-06-25 19:53:34 -0500

    added styling for MainContainer component

--- src/components/MainContainer.jsx
+++ src/components/MainContainer.jsx
@@ -1,15 +1,15 @@
 import styles from "./MainContainer.module.css";
 
 export default function MainContainer({ cat, handleClick }) {
-  console.log(cat)
+  console.log(cat.breeds[0].name)
   return (
     <div className={styles.container}>
-      <h2 className={styles.title}>{cat.name}</h2>
-      <div className={styles.featureContainer}>
-        <button className={styles.featurebtn}>{cat.breeds[0].name}</button>
-        <button className={styles.featurebtn}>{cat.breeds[0].weight.imperial} lbs</button>
-        <button className={styles.featurebtn}>{cat.breeds[0].origin}</button>
-        <button className={styles.featurebtn}>{cat.breeds[0].life_span} years</button>
+      <h2 className={styles.title}>{cat.breeds[0].name}</h2>
+      <div className={styles.featureBtnContainer}>
+        <button className={styles.featureBtn}>{cat.breeds[0].name}</button>
+        <button className={styles.featureBtn}>{cat.breeds[0].weight.imperial} lbs</button>
+        <button className={styles.featureBtn}>{cat.breeds[0].origin}</button>
+        <button className={styles.featureBtn}>{cat.breeds[0].life_span} years</button>
       </div>
       <img src={cat.url} alt={cat.breeds[0].name} className={styles.catImg} />
       <button className={styles.primaryBtn} onClick={handleClick}>Discover</button>
